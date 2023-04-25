a3c5da6ee624b2c5806184c76884c389380415bb
김선만 <seonman.kim@jnfirst.co.kr>
2023-04-25 13:57:04 +0900

    init

---
+++ src/main/java/com/example/graphqlexamplebykotlin/GraphQlExampleByKotlinApplication.java
@@ -0,0 +1,13 @@
+package com.example.graphqlexamplebykotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlExampleByKotlinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlExampleByKotlinApplication.class, args);
+    }
+
+}
